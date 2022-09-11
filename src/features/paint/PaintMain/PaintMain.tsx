442c94c4e7d93074b1d275eacfbf46bd7b322fc3
easybrizi <kiri4enkoalexandr@gmail.com>
2022-09-11 20:04:29 +0300

    init

---
+++ src/features/paint/PaintMain/PaintMain.tsx
@@ -0,0 +1,81 @@
+import { FC, useEffect, useRef, useState } from 'react';
+import { TOOL_TYPE, ToolInstanceType, ToolTypeName } from '../types';
+import { ToolBar } from '../ToolBar';
+import { TOOLS_WITH_CLASSIC_PROPERTIES } from '../config';
+import styles from './PaintMain.module.scss';
+
+export const PaintMain: FC = () => {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const [tool, setTool] = useState<ToolInstanceType | null>(null);
+
+  const [fillStyle, setFillStyle] =
+    useState<CanvasRenderingContext2D['fillStyle']>('#000000');
+
+  const [strokeStyle, setStrokeStyle] =
+    useState<CanvasRenderingContext2D['strokeStyle']>('#000000');
+
+  const [lineWidth, setLineWidth] = useState<number>(3);
+
+  useEffect(() => {
+    if (tool) {
+      tool.strokeColor = strokeStyle;
+      tool.fillColor = fillStyle;
+      tool.lineWidth = lineWidth;
+    }
+  }, [fillStyle, strokeStyle, lineWidth]);
+
+  useEffect(() => {
+    if (canvasRef.current) {
+      createTool('brush');
+    }
+  }, []);
+
+  const createTool = (toolType: ToolTypeName) => {
+    if (!canvasRef.current) {
+      return;
+    }
+    const tool = new TOOL_TYPE[toolType](canvasRef.current);
+
+    if (TOOLS_WITH_CLASSIC_PROPERTIES.includes(toolType)) {
+      tool.strokeColor = strokeStyle;
+      tool.fillColor = fillStyle;
+      tool.lineWidth = lineWidth;
+    } else if (toolType === 'eraser') {
+      tool.strokeColor = '#FFFFFF';
+      tool.fillColor = '#FFFFFFF';
+      tool.lineWidth = lineWidth;
+    }
+
+    setTool(tool);
+  };
+
+  const handleSetTool = (toolType: ToolTypeName) => {
+    if (!canvasRef.current) {
+      return;
+    }
+    createTool(toolType);
+  };
+
+  return (
+    <div className={styles.wrap}>
+      <div>
+        <ToolBar
+          setTool={handleSetTool}
+          setFillStyle={setFillStyle}
+          setStrokeStyle={setStrokeStyle}
+          lineWidth={lineWidth}
+          setLineWidth={setLineWidth}
+        />
+      </div>
+      <div>
+        <canvas
+          className={styles.canvas}
+          ref={canvasRef}
+          width={800}
+          height={600}
+          style={{ backgroundColor: 'white' }}
+        />
+      </div>
+    </div>
+  );
+};
---
+++ src/features/paint/ToolBar/ToolBar.tsx
@@ -0,0 +1,64 @@
+import { Dispatch, FC } from 'react';
+import { ToolTypeName } from '../types';
+import styles from './ToolBar.module.scss';
+
+interface ToolBarProps {
+  setTool: (toolType: ToolTypeName) => void;
+  setFillStyle: Dispatch<CanvasRenderingContext2D['fillStyle']>;
+  setStrokeStyle: Dispatch<CanvasRenderingContext2D['strokeStyle']>;
+  lineWidth: number;
+  setLineWidth: Dispatch<number>;
+}
+
+export const ToolBar: FC<ToolBarProps> = ({
+  setTool,
+  setStrokeStyle,
+  setFillStyle,
+  lineWidth,
+  setLineWidth,
+}) => {
+  return (
+    <div className={styles.wrap}>
+      <button className={styles.toolButton} onClick={() => setTool('brush')}>
+        brush
+      </button>
+
+      <button className={styles.toolButton} onClick={() => setTool('eraser')}>
+        eraser
+      </button>
+
+      <button className={styles.toolButton} onClick={() => setTool('circle')}>
+        circle
+      </button>
+
+      <button className={styles.toolButton} onClick={() => setTool('line')}>
+        line
+      </button>
+
+      <button className={styles.toolButton} onClick={() => setTool('rect')}>
+        rect
+      </button>
+
+      <span className={styles.toolColor}>
+        <label>fill</label>
+        <input type="color" onChange={(e) => setFillStyle(e.target.value)} />
+      </span>
+
+      <span className={styles.toolColor}>
+        <label>stroke</label>
+        <input type="color" onChange={(e) => setStrokeStyle(e.target.value)} />
+      </span>
+
+      <span className={styles.toolLineWidth}>
+        <label>lineWidth</label>
+        <input
+          type="number"
+          onChange={(e) => setLineWidth(Number.parseInt(e.target.value))}
+          value={lineWidth}
+          max={20}
+          min={1}
+        />
+      </span>
+    </div>
+  );
+};
